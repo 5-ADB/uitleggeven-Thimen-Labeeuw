56c732fab3908cc3aeea730b1781bb6aa2b2579d
Thimen-Labeeuw <Thimen.Labeeuw@student.tsaam.be>
2025-03-24 11:37:46 +0100

    v1

--- script/script-sponsors.js
+++ script/script-sponsors.js
@@ -1,57 +1,103 @@
 const sponsorlijst = [
-  { id: 1, type: "big", info: { img: "hubo.png", url: "https://www.hubo.be" } },
+  {
+    id: 1,
+    type: "big",
+    info: {
+      img: "hubo.png",
+      url: "https://www.hubo.be",
+      hidden: true,
+    },
+  },
   {
     id: 2,
     type: "big",
     info: {
       img: "vandamme.png",
       url: "https://www.vandamme-aanhangwagens.be/",
+      hidden: false,
     },
   },
   {
     id: 3,
     type: "big",
-    info: { img: "logoIchtegem.png", url: "https://www.ichtegem.be" },
+    info: {
+      img: "logoIchtegem.png",
+      url: "https://www.ichtegem.be",
+      hidden: false,
+    },
   },
   {
     id: 3,
     type: "big",
-    info: { img: "Lisalyn.png", url: "https://lisalyn.be/" },
+    info: {
+      img: "Lisalyn.png",
+      url: "https://lisalyn.be/",
+      hidden: false,
+    },
   },
   {
     id: 4,
     type: "middle",
-    info: { img: "psychio.png", url: "https://www.google.be" },
+    info: {
+      img: "psychio.png",
+      url: "https://www.google.be",
+      hidden: false,
+    },
   },
   {
     id: 5,
     type: "middle",
-    info: { img: "youreal.png", url: "https://www.google.be" },
+    info: {
+      img: "youreal.png",
+      url: "https://www.google.be",
+      hidden: false,
+    },
   },
   {
     id: 6,
     type: "middle",
-    info: { img: "KristofDeJaegher-Defaeye.png", url: "https://www.google.be" },
+    info: {
+      img: "KristofDeJaegher-Defaeye.png",
+      url: "https://www.google.be",
+      hidden: true,
+    },
   },
   {
-    id: 6,
+    id: 7,
     type: "small",
-    info: { img: "wit.png", url: "https://www.google.be" },
+    info: {
+      img: "wit.png",
+      url: "https://www.google.be",
+      hidden: false,
+    },
+    hidden: false,
   },
   {
-    id: 6,
+    id: 8,
     type: "small",
-    info: { img: "SBB.png", url: "https://www.google.be" },
+    info: {
+      img: "SBB.png",
+      url: "https://www.google.be",
+      hidden: false,
+    },
   },
   {
-    id: 6,
+    id: 9,
     type: "small",
-    info: { img: "okay.png", url: "https://www.google.be" },
+    info: {
+      img: "okay.png",
+      url: "https://www.google.be",
+      hidden: false,
+    },
   },
   {
-    id: 6,
+    id: 10,
     type: "small",
-    info: { img: "logge.jpg", url: "https://www.google.be" },
+    info: {
+      img: "logge.jpg",
+      url: "https://www.google.be",
+      hidden: true,
+    },
   },
 ];
 
@@ -64,6 +110,11 @@ sponsorlijst
   .forEach((sponsor) => {
     const dv = document.createElement("div");
     dv.style.backgroundImage = `url('./figuren/sponsors/${sponsor.info.img}')`;
+    if (sponsor.info.hidden == true) {
+      dv.style.display = "none";
+    } else {
+      dv.style.display = "block";
+    }
     big.appendChild(dv);
     dv.addEventListener("click", () => {
       openInNewTab(sponsor.info.url);
@@ -75,6 +126,11 @@ sponsorlijst
   .forEach((sponsor) => {
     const dv = document.createElement("div");
     dv.style.backgroundImage = `url('./figuren/sponsors/${sponsor.info.img}')`;
+    if (sponsor.info.hidden == true) {
+      dv.style.display = "none";
+    } else {
+      dv.style.display = "block";
+    }
     middle.appendChild(dv);
     dv.addEventListener("click", () => {
       openInNewTab(sponsor.info.url);
@@ -86,6 +142,11 @@ sponsorlijst
   .forEach((sponsor) => {
     const dv = document.createElement("div");
     dv.style.backgroundImage = `url('./figuren/sponsors/${sponsor.info.img}')`;
+    if (sponsor.info.hidden == true) {
+      dv.style.display = "none";
+    } else {
+      dv.style.display = "block";
+    }
     small.appendChild(dv);
     dv.addEventListener("click", () => {
       openInNewTab(sponsor.info.url);
